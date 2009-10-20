9521d69cfddcb52f67f1c7d64184bda77d067dd0
Adrian Thurston <thurston@colm.net>
2009-10-20 02:31:21 +0000

    Moving away, replacing with the CakePHP version.

--- spp/cake/app/webroot/index.php
+++ spp/cake/app/webroot/index.php
@@ -27,9 +27,11 @@
 /**
  * Use the DS to separate the directories in other defines
  */
-	if (!defined('DS')) {
-		define('DS', DIRECTORY_SEPARATOR);
-	}
+
+if (!defined('DS')) {
+	define('DS', DIRECTORY_SEPARATOR);
+}
+
 /**
  * These defines should only be edited if you have cake installed in
  * a directory layout other than the way it is distributed.
@@ -40,54 +42,86 @@
  * The full path to the directory which holds "app", WITHOUT a trailing DS.
  *
  */
-	if (!defined('ROOT')) {
-		define('ROOT', dirname(dirname(dirname(__FILE__))));
-	}
+if (!defined('ROOT')) {
+	define('ROOT', dirname(dirname(dirname(__FILE__))));
+}
+
 /**
  * The actual directory name for the "app".
  *
  */
-	if (!defined('APP_DIR')) {
-		define('APP_DIR', basename(dirname(dirname(__FILE__))));
-	}
+if (!defined('APP_DIR')) {
+	define('APP_DIR', basename(dirname(dirname(__FILE__))));
+}
+
 /**
  * The absolute path to the "cake" directory, WITHOUT a trailing DS.
  *
  */
-	if (!defined('CAKE_CORE_INCLUDE_PATH')) {
-		define('CAKE_CORE_INCLUDE_PATH', ROOT);
-	}
+
+if (!defined('CAKE_CORE_INCLUDE_PATH')) {
+	define('CAKE_CORE_INCLUDE_PATH', ROOT);
+}
 
 /**
  * Editing below this line should NOT be necessary.
  * Change at your own risk.
  *
  */
-	if (!defined('WEBROOT_DIR')) {
-		define('WEBROOT_DIR', basename(dirname(__FILE__)));
+
+if (!defined('WEBROOT_DIR')) {
+	define('WEBROOT_DIR', basename(dirname(__FILE__)));
+}
+
+if (!defined('WWW_ROOT')) {
+	define('WWW_ROOT', dirname(__FILE__) . DS);
+}
+
+if (!defined('CORE_PATH')) {
+	if (function_exists('ini_set') && ini_set('include_path', 
+			CAKE_CORE_INCLUDE_PATH . PATH_SEPARATOR . ROOT . DS . APP_DIR . 
+			DS . PATH_SEPARATOR . ini_get('include_path')))
+	{
+		define('APP_PATH', null);
+		define('CORE_PATH', null);
+	} else {
+		define('APP_PATH', ROOT . DS . APP_DIR . DS);
+		define('CORE_PATH', CAKE_CORE_INCLUDE_PATH . DS);
 	}
-	if (!defined('WWW_ROOT')) {
-		define('WWW_ROOT', dirname(__FILE__) . DS);
-	}
-	if (!defined('CORE_PATH')) {
-		if (function_exists('ini_set') && ini_set('include_path', CAKE_CORE_INCLUDE_PATH . PATH_SEPARATOR . ROOT . DS . APP_DIR . DS . PATH_SEPARATOR . ini_get('include_path'))) {
-			define('APP_PATH', null);
-			define('CORE_PATH', null);
-		} else {
-			define('APP_PATH', ROOT . DS . APP_DIR . DS);
-			define('CORE_PATH', CAKE_CORE_INCLUDE_PATH . DS);
+}
+
+if (!include(CORE_PATH . 'cake' . DS . 'bootstrap.php')) {
+	trigger_error("CakePHP core could not be found.  " . 
+		"Check the value of CAKE_CORE_INCLUDE_PATH in APP/webroot/index.php.  " .
+		"It should point to the directory containing your " . 
+		DS . "cake core directory and your " . DS . 
+		"vendors root directory.", E_USER_ERROR);
+}
+
+include( ROOT . "../../php/config.php" );
+
+if (isset($_GET['url']) && $_GET['url'] === 'favicon.ico') {
+	return;
+} else {
+
+	if ( isset( $_GET['url'] ) ) {
+		$url = $_GET['url'];
+		if ( preg_match( '/^[^\/]+\//', $url ) ) {
+			$url = preg_replace( '/^[^\/]+\//', '', $url );
+
+			$USER_NAME = isset( $_GET['u'] ) ? $_GET['u'] : "";
+			$USER_PATH = "${CFG_PATH}$USER_NAME/";
+			$USER_URI = "${CFG_URI}$USER_NAME/";
+
+			if ( $url == null || strlen( $url ) == 0 )
+				$url = "home";
 		}
 	}
-	if (!include(CORE_PATH . 'cake' . DS . 'bootstrap.php')) {
-		trigger_error("CakePHP core could not be found.  Check the value of CAKE_CORE_INCLUDE_PATH in APP/webroot/index.php.  It should point to the directory containing your " . DS . "cake core directory and your " . DS . "vendors root directory.", E_USER_ERROR);
-	}
-	if (isset($_GET['url']) && $_GET['url'] === 'favicon.ico') {
-		return;
-	} else {
-		$Dispatcher = new Dispatcher();
-		$Dispatcher->dispatch($url);
-	}
-	if (Configure::read() > 0) {
-		echo "<!-- " . round(getMicrotime() - $TIME_START, 4) . "s -->";
-	}
+
+	$Dispatcher = new Dispatcher();
+	$Dispatcher->dispatch($url);
+}
+if (Configure::read() > 0) {
+	echo "<!-- " . round(getMicrotime() - $TIME_START, 4) . "s -->";
+}
 ?>
