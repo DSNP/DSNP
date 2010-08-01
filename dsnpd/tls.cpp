467ef9947f9343dbf4cbbafe9e70abea36987162
Adrian Thurston <thurston@colm.net>
2010-08-01 03:53:14 +0000

    Reverted accidental commit.

--- dsnpd/tls.cpp
+++ dsnpd/tls.cpp
@@ -88,8 +88,8 @@ BIO *sslStartClient( BIO *readBio, BIO *writeBio, const char *host )
 	char peer_CN[256];
 	X509_NAME_get_text_by_NID( X509_get_subject_name(peer), NID_commonName, peer_CN, 256);
 
-	//if ( strcasecmp( peer_CN, host ) != 0 )
-	//	fatal("common name doesn't match host name\n");
+	if ( strcasecmp( peer_CN, host ) != 0 )
+		fatal("common name doesn't match host name\n");
 
 	/* Create a BIO for the ssl wrapper. */
 	BIO *sbio = BIO_new( BIO_f_ssl() );
