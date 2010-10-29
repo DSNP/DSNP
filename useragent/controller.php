e62eaa50c09cede150e77f0c2437c00f1c78018b
Adrian Thurston <thurston@colm.net>
2010-10-29 05:51:44 +0000

    Started on wrapping up the communication with the local DSNP server.

--- useragent/controller.php
+++ useragent/controller.php
@@ -1,10 +1,43 @@
 <?php
+class Connection
+{
+	var $fp;
+	var $result;
+
+	function openLocal()
+	{
+		global $CFG;
+
+		$this->fp = fsockopen( 'localhost', $CFG[PORT] );
+		if ( ! $this->fp )
+			exit(1);
+		
+		$send = 
+			"SPP/0.1 " . $CFG[URI] . "\r\n" .
+			"comm_key " . $CFG[COMM_KEY] . "\r\n";
+		fwrite( $this->fp, $send );
+	}
+
+	function command( $cmd )
+	{
+		fwrite( $this->fp, $cmd );
+		$this->result = fgets( $this->fp );
+	}
+
+	function checkResult( $pat )
+	{
+		if ( !ereg( $pat , $this->result ) ) {
+			die( "FAILURE *** New user creation failed with: <br> " .
+					$this->result );
+		}
+	}
+};
+
 class Controller
 {
 	var $controller = null;
 	var $method = null;
 	var $hasView = true;
-
 	var $CFG = null;
 
 	function Controller()
--- useragent/controller/site.php
+++ useragent/controller/site.php
@@ -23,20 +23,11 @@ class SiteController extends Controller
 		if ( $pass1 != $pass2 )
 			die("password mismatch");
 
-		$fp = fsockopen( 'localhost', $this->CFG[PORT] );
-		if ( !$fp )
-			exit(1);
+		$connection = new Connection;
+		$connection->openLocal();
+		$connection->command( "new_user $user $pass1\r\n" );
+		$connection->checkResult( "^OK" );
 
-		$send = 
-			"SPP/0.1 " . $this->CFG[URI] . "\r\n" . 
-			"comm_key " . $this->CFG[COMM_KEY] . "\r\n" .
-			"new_user $user $pass1\r\n";
-		fwrite($fp, $send);
-
-		$res = fgets($fp);
-		if ( !ereg("^OK", $res) )
-			die( "FAILURE *** New user creation failed with: <br> $res" );
-   	
 		$identity = $this->CFG[URI] . $user . '/';
 		dbQuery( "UPDATE user SET name = %e, identity = %e, " .
 			"type = %l WHERE user = %e ",
