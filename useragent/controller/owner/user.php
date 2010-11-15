98b355664ab86a4d8f77315d257a6d865f9d19b9
Adrian Thurston <thurston@colm.net>
2010-11-15 07:10:49 +0000

    Added user editing.

--- useragent/controller/owner/user.php
+++ useragent/controller/owner/user.php
@@ -12,7 +12,13 @@ class OwnerUserController extends Controller
 			array(
 				get => 'backto'
 			)
-		)
+		),
+		'edit' => array(),
+		'sedit' => array(
+			array( post => 'id' ),
+			array( post => 'name' ),
+			array( post => 'email' ),
+		),
 	);
 
 	function broadcast()
@@ -57,6 +63,61 @@ class OwnerUserController extends Controller
 		$reqid = $regs[1];
 
 		$this->redirect( "${backto}user/finish?reqid=$reqid" );
+	}
+
+	function edit()
+	{
+		$user = dbQuery( 
+			"SELECT * FROM user WHERE user = %e", 
+			$this->USER[USER] );
+		$this->vars['user'] = $user;
+	}
+
+	function sedit()
+	{
+		$id = $this->args['id'];
+		$name = $this->args['name'];
+		$email = $this->args['email'];
+
+		if ( preg_match( '/^[ \t\n]*$/', $name ) )
+			$name = null;
+		if ( preg_match( '/^[ \t\n]*$/', $email ) )
+			$email = null;
+
+		if ( $id === $this->USER[ID] ) {
+			dbQuery( 
+				"UPDATE user SET name = %e, email = %e WHERE id = %l",
+				$name, $email, $this->USER[ID] );
+		}
+
+		/* User message */
+		$headers = 
+			"Content-Type: text/plain\r\n" .
+			"Type: name-change\r\n" .
+			"\r\n";
+		$message = $this->data['User']['name'];
+		$len = strlen( $headers ) + strlen( $message );
+
+		$connection = new Connection;
+		$connection->openLocalPriv();
+		$connection->submitBroadcast( 
+			$this->USER[USER], '-', $len, $headers, $message );
+
+		if ( ereg("^OK", $connection->result, $regs) )
+			$this->userRedirect( "/" );
+		else
+			die( "submit_broadcast failed with $connection->result" );
+
+#		$this->VALID_USER = $this->Session->read('VALID_USER');
+
+#		if ( ! ereg("^OK", $res, $regs) )
+#			die( $res );
+
+		/* Invalidate the user data in the session so it is reloaded on the
+		 * next page load. */
+		#$this->Session->write( 'VALID_USER', false );
+
+		$this->userRedirect( "/" );
 	}
 }
 ?>
---
+++ useragent/view/owner/user/edit.php
@@ -0,0 +1,37 @@
+<table><tr>
+
+<td id="leftcol">
+<div id="details">
+<h2><?php echo $USER[USER];?></h2>
+</div>
+</td>
+
+<td id="activity">
+
+<div class="content">
+
+<form method="post" action="<? echo $this->userLoc('/user/sedit');?>">
+<fieldset style="display:none;"><input type="hidden" name="_method" value="PUT" /></fieldset>
+<div class="input text">
+	<label for="UserName">Name</label>
+	<input name="name" type="text" maxlength="50" 
+	value="<?php echo htmlspecialchars( $user[0]['name'] )?>" 
+	id="UserName" />
+</div>
+<div class="input text">
+	<label for="UserEmail">Email</label>
+	<input name="email" type="text" maxlength="50" value="" id="UserEmail" />
+</div>
+<input type="hidden" name="id" value="<? echo $this->USER[ID]; ?>" id="UserId" />
+<div class="submit">
+<input type="submit" value="Update" />
+</div>
+
+</form>
+
+</div>
+
+</td>
+
+</tr></table>
+
