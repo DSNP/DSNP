71b233ebba2cad4cc2611c7b8713795acabdc83a
Adrian Thurston <thurston@colm.net>
2011-01-18 04:18:35 +0000

    Bringing this dir back, just for the DSNP specification.

--- useragent/find.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#
-
-d="$1"
-[ -z "$d" ] && d=.
-
-find "$d" -name .svn -prune -or \( -type f -print \)
--- useragent/regexes.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define( 'REGEX_USER', '/^.+$/' );
-define( 'REGEX_ID',   '/^.+$/' );
-define( 'REGEX_DATE', '/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/' );
-define( 'REGEX_TIME', '/^[0-9]{2}:[0-9]{2}:[0-9]{2}$/' );
-
-?>
